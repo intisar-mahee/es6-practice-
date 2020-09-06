f425cb465380620f699580be1293cdbe3c345245
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-09-06 22:15:34 +0600

    final

---
+++ arrow.js
@@ -0,0 +1,14 @@
+const doubleIt = num => num*2;
+const add=(x,y)=> x+y;
+const give5=()=>5;
+const result = add(5,15);
+const result2= give5();
+
+const newMAth=(x,y)=>{
+    const sum = x+y;
+    const minus = x-y;
+    const final = sum*minus;
+    return final;
+}
+const result4 = newMAth(15, 10);
+console.log(result4);
