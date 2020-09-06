b733345e206f879f6687b1756d50c676243aaef2
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-09-06 21:58:07 +0600

    third

---
+++ template.js
@@ -0,0 +1,10 @@
+const firstName = "Justin";
+const lastNAme = "Timberlake";
+const fullName = firstName + " " + lastNAme;
+
+const fullName2 = `${firstName} ${lastNAme} is a goodboy`;
+const fullName3 = `I love you
+I hate you
+I need you
+fuck you`
+console.log(fullName3); 
