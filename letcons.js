755b8b35fbd3d92054afe764386a1bb478a0836f
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-09-06 20:47:56 +0600

    one

---
+++ letcons.js
@@ -0,0 +1,10 @@
+const hubby = "elias";
+console.log(hubby); 
+
+const num = [25, 30, 66];
+num[1]= 88;
+num.push(69);
+
+const nayok= {name: "sakib khan", number:76545};
+console.log(nayok);
+console.log(num);
